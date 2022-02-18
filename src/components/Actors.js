ab45c80c0cc0c544f5d1f7381980847d69579ceb
Micaella Colombo <micaelc@g.clemson.edu>
2022-02-17 19:48:46 -0500

    commit tree b5144e301b38afcb80f30e8ace256df824b412d3

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,16 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>;
+      {actors.map((a) => 
+        <div>
+            <h4>Actor:{a.name}</h4>
+            <h4>Movies:</h4>
+              <ul> 
+                {a.movies.map( (movie) => (<li>{movie}</li> ))}
+              </ul>
+        </div>
+      )}
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,24 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map((movie) => {
+          return (
+            <div>
+              <p>
+                {movie.title}<br/>
+                {movie.time}
+                <ul>
+                  {movie.genres.map((genre) => {
+                    return (
+                      <li>{genre}</li>
+                    );
+                  })}
+                </ul>
+              </p>
+            </div>
+          );
+        })}
     </div>
   );
 };
